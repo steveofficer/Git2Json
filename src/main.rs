dcdadc45c7ccebe5e098c92e801f827b58056faa
Steve <steve.officer@gmail.com>
2024-03-07 20:37:51 +0000

    Second commit

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,21 @@
 use std::process::Command;
+use std::process::Output;
 use std::str;
 
+fn handle_git_output(output: &Output) {
+    if output.status.success() {
+        println!("Git output {:?}", str::from_utf8(&output.stdout))
+    } else {
+        println!("Git error {:?}", str::from_utf8(&output.stderr))
+    }
+}
+
 fn main() {
-    let output = Command::new("git")
+    let git_result = Command::new("git")
       .args([ "log", "-1" ])
       .output();
-    match output {
-        | Ok(x) => println!("Ok {:?}", str::from_utf8(&x.stderr)),
-        | Err(e) => println!("Error {}", e) 
+    match git_result {
+        | Ok(git_output) => handle_git_output(&git_output),
+        | Err(err) => println!("Error {}", err) 
     }
 }
