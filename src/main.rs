06473a6bd4417920faaa8c2244efb11d1837847b
Steve <steve.officer@gmail.com>
2024-03-11 14:16:36 +0000

    Added some more fleshed out parsing for git objects It turns out that tree objects aren't just plain text, they have binary encoded content that needs to be handled very specifically.
    Git Commits and Blobs do seem to be plain text though.

--- src/main.rs
+++ src/main.rs
@@ -1,21 +1,43 @@
-use std::process::Command;
-use std::process::Output;
+mod git_parser;
+
+use std::io;
+use std::fs::read_to_string;
+
 use std::str;
 
-fn handle_git_output(output: &Output) {
-    if output.status.success() {
-        println!("Git output {:?}", str::from_utf8(&output.stdout))
-    } else {
-        println!("Git error {:?}", str::from_utf8(&output.stderr))
-    }
+fn find_head_ref(directory_name: &str) -> io::Result<String> {
+    // The content .git/HEAD of points us to a file that contains the HEAD commit.
+    let head_content= read_to_string(format!("{}/.git/HEAD", directory_name))?;
+    // Drop the leading "ref: " and get the actual file name 
+    let head_ref = &head_content[5..];
+
+    // Get the commit from that file
+    let full_ref = format!("{}/.git/{}", directory_name, head_ref.trim_end());
+    
+    let head_commit = read_to_string(full_ref)?;
+    
+    Ok(String::from(head_commit.trim_end()))
+}
+
+fn hash_to_file_path(hash: &str) -> (&str, &str) {
+    (&hash[..2], &hash[2..])
 }
 
 fn main() {
-    let git_result = Command::new("git")
-      .args([ "log", "-1" ])
-      .output();
-    match git_result {
-        | Ok(git_output) => handle_git_output(&git_output),
-        | Err(err) => println!("Error {}", err) 
-    }
+    let head_commit = find_head_ref("C:/Dev/Git2Json");
+    let head_content = head_commit.expect("Failed to read starting commit");
+    println!("Starting with commit {:?}", head_content);
+
+    let (folder, path) = hash_to_file_path(&head_content);
+    let head_file_path = format!("C:/Dev/Git2Json/.git/objects/{}/{}", folder, path);
+    println!("Reading {}", head_file_path);
+    
+    let content = git_parser::read_git_file(&head_file_path);
+    println!("HEAD commit content\n{:?}", content);
+
+    let (folder, path) = hash_to_file_path("18ca9cadb6d1383000eb95ef956f8aa7c99c1680");
+    let file_path = format!("C:/Dev/Git2Json/.git/objects/{}/{}", folder, path);
+    println!("{}", file_path);
+    println!("{:?}", git_parser::read_git_file(&file_path));
+
 }
